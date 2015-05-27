0a8ed29203d34744a501a9555d24d0191f3ef574
u12211070 <u12211070@tuks.co.za>
2015-05-27 09:50:12 +0200

    new files

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<title>INF354 home page</title>
+	</head>
+	<body>
+		<h1>Home page</h1>
+		<h3>Mphahlele Collins</h3>
+		<p>12211070</p>
+		<img src="index.jpeg"/>
+	</body>
+</html>
